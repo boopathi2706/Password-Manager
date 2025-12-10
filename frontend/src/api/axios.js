957e1f2ec54aa90df607ae3cf386ec323a400a0d
boopathi2706 <boopathi.v2023cse@sece.ac.in>
2025-12-10 12:35:42 +0530

    first commit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // <--- MUST match backend port and /api prefix
+    baseURL: 'https://password-manager-rwy8.onrender.com/api', // <--- MUST match backend port and /api prefix
     withCredentials: true,
 });
 
